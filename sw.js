fa67072ed0b0fa21952beb9a25f3a04145d30f33
GitHub Action <action@github.com>
2025-12-06 11:55:32 +0000

    Bump version to 2025.1.28

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // AUTO-UPDATED BY CI
-const APP_VERSION = '2025.1.27';
-const BUILD_NUMBER = 27;
+const APP_VERSION = '2025.1.28';
+const BUILD_NUMBER = 28;
 const CACHE_NAME = `codeblur-${APP_VERSION}`;
 const urlsToCache = [
   '/',
